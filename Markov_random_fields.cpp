e29e2332aea1b5650e9fe5ee931921fc45726ee4
Kartik-Agra-wal <87862080+Kartik-Agra-wal@users.noreply.github.com>
2023-08-24 01:43:50 +0530

    Update Markov_random_fields.cpp

--- Markov_random_fields.cpp
+++ Markov_random_fields.cpp
@@ -3,54 +3,29 @@
 #include <maxflow-v3.03/graph.h>
 
 int main() {
-    int numPixels = 100;  // Number of pixels
+    int numPixelsX = 10;  // Number of pixels along X-axis
+    int numPixelsY = 10;  // Number of pixels along Y-axis
     int numDisparities = 16;  // Number of possible disparity values
     
     // Simulated data costs (negative log-likelihoods)
-    std::vector<std::vector<int>> dataCosts(numPixels, std::vector<int>(numDisparities, 0));
-    
-    // Simulated smoothness costs (pairwise potentials)
-    std::vector<std::vector<int>> smoothnessCosts(numDisparities, std::vector<int>(numDisparities, 0));
+    std::vector<std::vector<std::vector<int>>> dataCosts(numPixelsY,
+        std::vector<std::vector<int>>(numPixelsX, std::vector<int>(numDisparities, 0)));
     
     // Create a graph
-    Graph<int, int, int> g(numPixels, numDisparities * numPixels);
+    Graph<int, int, int> g(numPixelsX * numPixelsY, numDisparities * numPixelsX * numPixelsY);
     
     // Add nodes with data costs
-    for (int pixel = 0; pixel < numPixels; ++pixel) {
-        for (int disparity = 0; disparity < numDisparities; ++disparity) {
-            g.add_node();
-            g.add_tweights(pixel * numDisparities + disparity, dataCosts[pixel][disparity], 0);
-        }
-    }
-    
-    // Add edges with smoothness costs (pairwise potentials)
-    for (int pixel = 0; pixel < numPixels - 1; ++pixel) {
-        for (int disparityA = 0; disparityA < numDisparities; ++disparityA) {
-            for (int disparityB = 0; disparityB < numDisparities; ++disparityB) {
-                g.add_edge(pixel * numDisparities + disparityA,
-                           (pixel + 1) * numDisparities + disparityB,
-                           smoothnessCosts[disparityA][disparityB],
-                           smoothnessCosts[disparityA][disparityB]);
+    for (int y = 0; y < numPixelsY; ++y) {
+        for (int x = 0; x < numPixelsX; ++x) {
+            for (int disparity = 0; disparity < numDisparities; ++disparity) {
+                g.add_node();
+                g.add_tweights(y * numPixelsX + x + disparity * numPixelsX * numPixelsY,
+                               dataCosts[y][x][disparity], 0);
             }
         }
     }
     
-    // Run the graph cut algorithm
-    int flow = g.maxflow();
-    
-    // Obtain the optimal disparity values
-    std::vector<int> optimalDisparities(numPixels);
-    for (int pixel = 0; pixel < numPixels; ++pixel) {
-        for (int disparity = 0; disparity < numDisparities; ++disparity) {
-            if (g.what_segment(pixel * numDisparities + disparity) == Graph<int, int, int>::SOURCE)
-                optimalDisparities[pixel] = disparity;
-        }
-    }
-    
-    // Print the optimal disparity values
-    for (int pixel = 0; pixel < numPixels; ++pixel) {
-        std::cout << "Pixel " << pixel << ": Disparity " << optimalDisparities[pixel] << std::endl;
-    }
-    
-    return 0;
-}
+    // Add horizontal edges with smoothness costs
+    for (int y = 0; y < numPixelsY; ++y) {
+        for (int x = 0; x < numPixelsX - 1; ++x) {
+           
